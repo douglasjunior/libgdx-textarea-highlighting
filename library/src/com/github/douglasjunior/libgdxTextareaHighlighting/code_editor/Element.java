cc8321fc516d744a0a776c715a4132f09793edcf
Douglas Nassif Roma Junior <nassifrroma@gmail.com>
2016-06-08 20:40:46 -0300

    first commit

---
+++ library/src/com/github/douglasjunior/libgdxTextareaHighlighting/code_editor/Element.java
@@ -0,0 +1,29 @@
+package com.github.douglasjunior.libgdxTextareaHighlighting.code_editor;
+
+import com.macbury.r0x16.widgets.JavaScriptScanner;
+import com.macbury.r0x16.widgets.JavaScriptScanner.Kind;
+
+public class Element {
+  public JavaScriptScanner.Kind kind;
+  public String text;
+  
+  public Element(Kind k, String t) {
+    text = t;
+    kind = k;
+  }
+
+  public int countSpaces() {
+    int sum = 0;
+    
+    for (int i = 0; i < text.length(); i++) {
+      char c = text.charAt(i);
+      if (c == ' ') {
+        sum += 1;
+      } else {
+        break;
+      }
+    }
+    
+    return sum;
+  }
+}
---
+++ library/src/com/github/douglasjunior/libgdxTextareaHighlighting/code_editor/Line.java
@@ -0,0 +1,56 @@
+package com.github.douglasjunior.libgdxTextareaHighlighting.code_editor;
+
+import com.macbury.r0x16.widgets.JavaScriptScanner;
+
+import java.util.ArrayList;
+
+public class Line extends ArrayList<Element> {
+  private String cachedFullText = "";
+  public int textLenght() {
+    return text().length();
+  }
+
+  public String text() {
+    String s = "";
+    for (Element e : this) {
+      s += e.text;
+    }
+    return s;
+  }
+  
+  public char charAt(int col) {
+    try {
+      return text().charAt(col);
+    } catch(StringIndexOutOfBoundsException e) {
+      return ' ';
+    }
+  }
+
+  public void buildString() {
+    setCachedFullText(text());
+  }
+
+  public String getCachedFullText() {
+    return cachedFullText;
+  }
+
+  public void setCachedFullText(String cachedFullText) {
+    this.cachedFullText = cachedFullText;
+  }
+
+  public int getPadding() {
+    int sum = 0;
+    for (int i = 0; i < this.size(); i++) {
+      Element e = this.get(i);
+      if (e.kind == JavaScriptScanner.Kind.NEWLINE) {
+        continue;
+      } else if (e.kind == JavaScriptScanner.Kind.NORMAL) {
+        sum += e.countSpaces();
+      } else {
+        break;
+      }
+    }
+    
+    return sum;
+  }
+}
